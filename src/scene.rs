fdf0d58cce88c22e1c600b616f02275dfbc2e4e1
Emil Ahlbäck <emil.ahlback99@gmail.com>
2020-04-09 01:01:38 +0200

    first attempt (doesnt work)

---
+++ src/scene.rs
@@ -0,0 +1,20 @@
+use crate::vector::Vector3;
+
+pub struct Color {
+    pub red: f32,
+    pub green: f32,
+    pub blue: f32,
+}
+
+pub struct Sphere {
+    pub center: Vector3,
+    pub radius: f64,
+    pub color: Color,
+}
+
+pub struct Scene {
+    pub width: u32,
+    pub height: u32,
+    pub fov: f64,
+    pub sphere: Sphere,
+}
---
+++ src/vector.rs
@@ -0,0 +1,107 @@
+use std::ops::{Add, Div, Mul, Sub};
+
+#[derive(Copy, Clone)]
+pub struct Vector3 {
+    pub x: f64,
+    pub y: f64,
+    pub z: f64,
+}
+
+impl Vector3 {
+    pub fn zero() -> Vector3 {
+        Vector3 {
+            x: 0.0,
+            y: 0.0,
+            z: 0.0,
+        }
+    }
+
+    pub fn length(&self) -> f64 {
+        self.norm().sqrt()
+    }
+
+    pub fn norm(&self) -> f64 {
+        self.x * self.x + self.y * self.y + self.z * self.z
+    }
+
+    pub fn normalize(self) -> Vector3 {
+        let len = self.length();
+        self / len
+    }
+
+    pub fn dot(&self, other: &Vector3) -> f64 {
+        self.x * other.x + self.y * other.y +self.z * other.z
+    }
+}
+
+impl Add for Vector3 {
+    type Output = Vector3;
+
+    fn add(self, other: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
+}
+
+impl Sub for Vector3 {
+    type Output = Vector3;
+
+    fn sub(self, other: Vector3) -> Vector3 {
+        Vector3 {
+            x: self.x - other.x,
+            y: self.y - other.y,
+            z: self.z - other.z,
+        }
+    }
+}
+
+impl Mul<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn mul(self, scalar: f64) -> Vector3 {
+        Vector3 {
+            x: self.x * scalar,
+            y: self.y * scalar,
+            z: self.z * scalar,
+        }
+    }
+}
+
+impl Mul<Vector3> for f64 {
+    type Output = Vector3;
+
+    fn mul(self, vector: Vector3) -> Vector3 {
+        Vector3 {
+            x: vector.x * self,
+            y: vector.y * self,
+            z: vector.z * self,
+        }
+    }
+}
+
+impl Div<f64> for Vector3 {
+    type Output = Vector3;
+
+    fn div(self, scalar: f64) -> Vector3 {
+        Vector3 {
+            x: self.x / scalar,
+            y: self.y / scalar,
+            z: self.z / scalar,
+        }
+    }
+}
+
+impl Div<&Vector3> for f64 {
+    type Output = Vector3;
+
+    fn div(self, vector: &Vector3) -> Vector3 {
+        Vector3 {
+            x: vector.x / self,
+            y: vector.y / self,
+            z: vector.z / self,
+        }
+    }
+}
